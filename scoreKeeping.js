a9a9efacc4f173ffceaa22cee01d445423d000fa
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-02-17 21:32:34 +0700

    adding updating live score feature

--- scoreKeeping.js
+++ scoreKeeping.js
@@ -3,3 +3,14 @@ const p2 = document.querySelector('#p2Score')
 const p1Add = document.querySelector('#p1Add')
 const p2Add = document.querySelector('#p2Add')
 const reset = document.querySelector('#reset')
+
+p1Add.addEventListener('click', function () {
+    let newValue = parseInt(p1.innerText) + 1
+
+    p1.innerText = newValue
+
+})
+p2Add.addEventListener('click', function () {
+    let newValue2 = parseInt(p2.innerText) + 1
+    p2.innerText = newValue2
+})
