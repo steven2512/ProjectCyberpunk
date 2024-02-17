3a2bd0505291b596cdbde6f55868ec081f531dd1
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-02-17 21:46:17 +0700

    fixed some id issues for the options in the select tag

--- scoreKeeping.js
+++ scoreKeeping.js
@@ -3,7 +3,7 @@ const p2 = document.querySelector('#p2Score')
 const p1Add = document.querySelector('#p1Add')
 const p2Add = document.querySelector('#p2Add')
 const reset = document.querySelector('#reset')
-
+const maxScore = document.querySelector('#maxScore')
 p1Add.addEventListener('click', function () {
     let newValue = parseInt(p1.innerText) + 1
 
@@ -14,3 +14,7 @@ p2Add.addEventListener('click', function () {
     let newValue2 = parseInt(p2.innerText) + 1
     p2.innerText = newValue2
 })
+
+//maxScore.addEventListener('input', function (e) {
+    
+//})
