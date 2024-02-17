c641e5fbde2034705d6e5eea2120415893317645
Nguyen Duong <128664583+steven2512@users.noreply.github.com>
2024-02-17 22:04:31 +0700

    fixed the maxScore issue

--- scoreKeeping.js
+++ scoreKeeping.js
@@ -4,18 +4,30 @@ const p1Add = document.querySelector('#p1Add')
 const p2Add = document.querySelector('#p2Add')
 const reset = document.querySelector('#reset')
 const maxScore = document.querySelector('#maxScore')
+let gameIsOver = false
 p1Add.addEventListener('click', function () {
-    let newValue = parseInt(p1.innerText) + 1
-    p1.innerText = newValue
-    if (parseInt(p1.innerText) === parseInt(maxScore.value)) {
-        p1.style.color = 'green';
-        p2.style.color = 'red';
+    if (!gameIsOver) {
+        let newValue = parseInt(p1.innerText) + 1
+        p1.innerText = newValue
+        if (parseInt(p1.innerText) === parseInt(maxScore.value)) {
+            p1.style.color = 'green';
+            p2.style.color = 'red';
+            gameIsOver = true
+        }
     }
 
 })
 p2Add.addEventListener('click', function () {
-    let newValue2 = parseInt(p2.innerText) + 1
-    p2.innerText = newValue2
+    if (!gameIsOver) {
+        let newValue2 = parseInt(p2.innerText) + 1
+        p2.innerText = newValue2
+        if (parseInt(p2.innerText) === parseInt(maxScore.value)) {
+            p2.style.color = 'green';
+            p1.style.color = 'red';
+            gameIsOver = true
+
+        }
+    }
 })
 
 
